66eef63a78e8f9e7ccd925b5b6dc33966464e0cf
Toshiki A <tarita141@gmail.com>
2014-11-05 00:55:28 -0800

    Temporary contact page

---
+++ contactinfo.php
@@ -0,0 +1,16 @@
+/**
+ * Created by PhpStorm.
+ * User: tarita
+ * Date: 11/5/14
+ * Time: 12:41 AM
+ */
+
+
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
---
+++ contactme.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+
+<form action="contactinfo.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
+
+
+</body>
+</html>
